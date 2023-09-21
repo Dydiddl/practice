7f8cc0b84efaaf1ec204fa6047938d8088efa07f
Dydiddl <dydiddl@naver.com>
2023-09-21 23:27:17 +0900

    Forder Organization

--- python/Practice_11_20/15_def_2_prac.py
+++ python/Practice_11_20/15_def_2_prac.py
@@ -11,5 +11,5 @@ def profile(name, age=17, main_language="Python"):
     print("이름 : {0}\t나이 : {1}\t주 사용 언어 : {2}" \
           .format(name, age, main_language))
     
-profile("유재석", 100)
+profile("유재석")
 profile("김태호")
